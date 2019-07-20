d0dd5298d18a42f8476d1f9d97d60f0b90482f04
Andy <andyjpagdin@gmail.com>
2019-07-20 15:03:17 +0100

    Initial commit

---
+++ public/electron.js
@@ -0,0 +1,32 @@
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const electron = require("electron")
+const path = require("path")
+const isDev = require("electron-is-dev")
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({ width: 900, height: 680 })
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  )
+  win.on("closed", () => (win = null))
+  win.webContents.openDevTools()
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit()
+  }
+})
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow()
+  }
+})
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h4>React electron app!</h4>
+    </div>
+  )
+}
+
+export default App
